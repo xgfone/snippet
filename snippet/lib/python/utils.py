a05392e73bf6f0c8f270afeb05e96a6839f9bed4
xgfone <xgfone@126.com>
2015-09-08 09:55:29 +0800

    Add the check_output function of subprocess.

--- snippet/lib/python/utils.py
+++ snippet/lib/python/utils.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import sys
 import traceback
+import subprocess
 
 # On Python(>=2.7), sys.version_info[0] <==> sys.version_info.major
 if sys.version_info[0] == 2:
@@ -129,3 +130,41 @@ def remove_builtins(args):
             raise AttributeError("{0} doesn't exist".format(arg))
         delattr(builtins, arg)
         _CACHE_BUILTIN.pop(arg)
+
+
+if getattr(subprocess, "check_output", None):
+    check_output = subprocess.check_output
+else:
+    def check_output(*popenargs, **kwargs):
+        r"""Run command with arguments and return its output as a byte string.
+
+        If the exit code was non-zero it raises a CalledProcessError.  The
+        CalledProcessError object will have the return code in the returncode
+        attribute and output in the output attribute.
+
+        The arguments are the same as for the Popen constructor.  Example:
+
+        >>> check_output(["ls", "-l", "/dev/null"])
+        'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+        The stdout argument is not allowed as it is used internally.
+        To capture standard error in the result, use stderr=STDOUT.
+
+        >>> check_output(["/bin/sh", "-c",
+        ...               "ls -l non_existent_file ; exit 0"],
+        ...              stderr=STDOUT)
+        'ls: non_existent_file: No such file or directory\n'
+        """
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd, output=output)
+        return output
+
+    subprocess.check_output = check_output
