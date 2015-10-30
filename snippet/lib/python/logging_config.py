08eb037d59f72259e1a0e15f4cf27fb144ab1416
xgfone <xgfone@126.com>
2015-10-30 13:25:39 +0800

    Update the dict config of logging.

--- snippet/lib/python/logging_config.py
+++ snippet/lib/python/logging_config.py
@@ -1,8 +1,6 @@
 # encoding: utf-8
 
-PROJECT = "PROJECT"
-
-LOGGING = {
+DEFAULT_LOGGING = {
     "version": 1,
     "incremental": False,  # Replaces the existing configuration
     "root": {
@@ -12,7 +10,7 @@ LOGGING = {
         "filters": [],
     },
     "loggers": {
-        PROJECT: {
+        "default": {
             "level": "INFO",
             "propagate": 0,
             "handlers": ["file", "console"],
@@ -48,10 +46,52 @@ LOGGING = {
             "filters": [],
 
             # Arguments of handler
-            "filename": "/var/log/{PROJECT}/{PROJECT}.log".format(PROJECT=PROJECT),
+            "filename": "/var/log/{PROJECT}/{PROJECT}.log",
             "when": "D",  # Backup once each day.
             "interval": 1,
             "backupCount": 31,  # The total number to backup.
         }
     }
 }
+
+
+def get_config(project, config=None):
+    import copy
+    _config = copy.deepcopy(DEFAULT_LOGGING)
+
+    file_handler = _config["handlers"]["file"]
+    file_handler["filename"] = file_handler["filename"].format(PROJECT=project)
+    _config["loggers"][project] = _config["loggers"]["default"]
+
+    if not isinstance(config, dict):
+        return _config
+
+    version = config.get("version", None)
+    if version is not None:
+        _config["version"] = version
+
+    incremental = config.get("incremental", None)
+    if incremental is not None:
+        _config["incremental"] = incremental
+
+    root = config.get("root", None)
+    if root is not None:
+        _config["root"] = root
+
+    loggers = config.get("loggers", {})
+    for k, v in loggers.items():
+        _config["loggers"][k] = v
+
+    formatters = config.get("formatters", {})
+    for k, v in formatters.items():
+        _config["formatters"][k] = v
+
+    handlers = config.get("handlers", {})
+    for k, v in handlers.items():
+        _config["handlers"][k] = v
+
+    filters = config.get("filters", {})
+    for k, v in filters.items():
+        _config["filters"][k] = v
+
+    return _config
