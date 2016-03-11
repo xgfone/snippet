afb40cdfc52a68947433cb732aa6124d6a90f2df
xgfone <xgfone@126.com>
2016-03-11 14:04:32 +0800

    Modify the logging configuration in Python Example

--- snippet/example/python/project/project/main.py
+++ snippet/example/python/project/project/main.py
@@ -12,14 +12,25 @@ from oslo_log import log
 CONF = cfg.CONF
 __VERSION__ = "0.1"
 
+project_opts = [
+    cfg.StrOpt("logging_config_file", default="",
+               help="The configuration file of logging for the {PROJECT}"),
+]
+CONF.register_cli_options(project_opts, group="{PROJECT}")
+
 
 def main(project="example"):
     log.register_options(CONF)
     # log.set_defaults(default_log_levels=None)
     CONF(project=project, version=__VERSION__)
-    log.setup(CONF, project, __VERSION__)
+
+    # (TODO) Daemon
 
     eventlet.monkey_patch(all=True)
+    if CONF.{PROJECT}.logging_config_file:
+        log._load_log_config(CONF.{PROJECT}.logging_config_file)
+    else:
+        log.setup(CONF, project, __VERSION__)
 
     # (TODO)
     pass
