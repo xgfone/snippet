22f72789fd3605b36858f5a61e9818bbdb5ee390
xgfone <xgfone@126.com>
2016-04-08 19:27:42 +0800

    Fix a bug

--- snippet/docs/tool/script/install-aria2-dep.sh
+++ snippet/docs/tool/script/install-aria2-dep.sh
@@ -1,2 +1,2 @@
-aptitude libtool autoconf autopoint
+aptitude install libtool autoconf autopoint
 aptitude install libgnutls-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config libssl-dev
