1b4207a2cf5f130dbf2467dfacd4789414d07dab
xgfone <xgfone@126.com>
2016-12-30 10:56:45 +0800

    Update python project example

--- snippet/example/python/project/project/db/sqlalchemy/models.py
+++ snippet/example/python/project/project/db/sqlalchemy/models.py
@@ -5,7 +5,9 @@ import logging
 
 from sqlalchemy.ext.declarative import declarative_base
 from oslo_db.sqlalchemy import models
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.sql import fun
 
 LOG = logging.getLogger(__name__)
 BASE = declarative_base()
@@ -16,8 +18,23 @@ class TestData(models.ModelBase, BASE):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     data = Column(String(256), nullable=False)
+    create_time = Column(DateTime, server_default=func.now(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super(TestData, self).__init__()
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+def create_tables(engine=None):
+    if not engine:
+        try:
+            import sys
+            engine = sys.argv[1]
+        except IndexError:
+            engine = "sqlite:///:memory:"
+    engine = create_engine(engine, echo=True)
+    BASE.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    create_tables("sqlite:///:memory:")
