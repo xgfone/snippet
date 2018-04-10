d1ff5dc5246943beacfbdca52d17eba5a40d6f0f
xgfone <xgfone@126.com>
2018-04-10 10:06:15 +0800

    Update python example xml2json

--- snippet/example/python/xml2json.py
+++ snippet/example/python/xml2json.py
@@ -1,3 +1,30 @@
+"""xml2json.py  Convert XML to JSON
+Relies on ElementTree for the XML parsing.  This is based on
+pesterfish.py but uses a different XML->JSON mapping.
+The XML->JSON mapping is described at
+http://www.xml.com/pub/a/2006/05/31/converting-between-xml-and-json.html
+Rewritten to a command line utility by Hay Kranen < github.com/hay > with
+contributions from George Hamilton (gmh04) and Dan Brown (jdanbrown)
+XML                              JSON
+<e/>                             "e": null
+<e>text</e>                      "e": "text"
+<e name="value" />               "e": { "@name": "value" }
+<e name="value">text</e>         "e": { "@name": "value", "#text": "text" }
+<e> <a>text</a ><b>text</b> </e> "e": { "a": "text", "b": "text" }
+<e> <a>text</a> <a>text</a> </e> "e": { "a": ["text", "text"] }
+<e> text <a>text</a> </e>        "e": { "#text": "text", "a": "text" }
+This is very similar to the mapping used for Yahoo Web Services
+(http://developer.yahoo.com/common/json.html#xml).
+This is a mess in that it is so unpredictable -- it requires lots of testing
+(e.g. to see if values are lists or strings or dictionaries).  For use
+in Python this could be vastly cleaner.  Think about whether the internal
+form can be more self-consistent while maintaining good external
+characteristics for the JSON.
+Look at the Yahoo version closely to see how it works.  Maybe can adopt
+that completely if it makes more sense...
+R. White, 2006 November 6
+"""
+
 import json
 
 try:
@@ -161,3 +188,50 @@ def xml2dict(xmlstring, strip_ns=1, strip=1):
 def dict2xml(dict_data, factory=ET.Element):
     elem = internal_to_elem(dict_data, factory)
     return ET.tostring(elem)
+
+
+if __name__ == "__main__":
+    import sys
+    import argparse
+
+    p = argparse.ArgumentParser(prog='xml2json',
+                                usage='%(prog)s -t xml2json -o file.json [file]',
+                                description=('Converts XML to JSON or the other way around. '
+                                             'Reads from standard input by default, '
+                                             'or from file if given.'))
+    p.add_argument('--type', '-t', help="'xml2json' or 'json2xml'", default="xml2json")
+    p.add_argument('--out', '-o', help="Write to the file 'OUT' instead of stdout")
+    p.add_argument('--fin', '-f', help="Read from the file 'IN' instead of stdin")
+    p.add_argument('--strip_text', action="store_true",
+                   dest="strip_text", help="Strip text for xml2json")
+    p.add_argument('--pretty', action="store_true",
+                   dest="pretty", help="Format JSON output so it is easier to read")
+    p.add_argument('--strip_namespace', action="store_true",
+                   dest="strip_ns", help="Strip namespace for xml2json")
+    p.add_argument('--strip_newlines', action="store_true",
+                   dest="strip_nl", help="Strip newlines for xml2json")
+
+    args = p.parse_args()
+    if args.fin:
+        with open(args.fin) as f:
+            data = f.read()
+    else:
+        data = sys.stdin.readline()
+    strip = 0
+    strip_ns = 0
+    if args.strip_text:
+        strip = 1
+    if args.strip_ns:
+        strip_ns = 1
+    if args.strip_nl:
+        data = data.replace('\n', '').replace('\r', '')
+    if args.type == "xml2json":
+        out = xml2json(data, args.pretty, strip_ns, strip)
+    else:
+        out = json2xml(data)
+    if args.out:
+        file = open(args.out, 'w')
+        file.write(out)
+        file.close()
+    else:
+        print(out)
